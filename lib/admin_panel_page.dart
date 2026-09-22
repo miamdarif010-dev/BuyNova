b3dab624b9f82ed59d13cdbd67d810c44a1f8c8f
miamdarif010-dev <miamdarif010@gmail.com>
2026-09-23 00:24:10 +0900

    Update admin_panel_page.dart

--- lib/admin_panel_page.dart
+++ lib/admin_panel_page.dart
@@ -300,7 +300,7 @@ class _AdminPanelPageState extends State<AdminPanelPage> {
                   width: 62,
                   height: 62,
                   decoration: BoxDecoration(
-                    color: Colors.white.withOpacity(0.75),
+                    color: Colors.white.withValues(alpha: 0.75),
                     shape: BoxShape.circle,
                   ),
                   child: const Icon(
@@ -720,7 +720,7 @@ class _AdminPanelPageState extends State<AdminPanelPage> {
         borderRadius: BorderRadius.circular(20),
         boxShadow: [
           BoxShadow(
-            color: Colors.black.withOpacity(0.04),
+            color: Colors.black.withValues(alpha: 0.04),
             blurRadius: 8,
             offset: const Offset(0, 3),
           ),
