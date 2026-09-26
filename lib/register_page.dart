84b41e79fac1602b9843110d6dac5561b4ebb009
miamdarif010-dev <miamdarif010@gmail.com>
2026-09-26 11:21:33 +0900

    Update register_page.dart

--- lib/register_page.dart
+++ lib/register_page.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 
-import 'home_page.dart';
+
 
 class RegisterPage extends StatefulWidget {
   const RegisterPage({super.key});
